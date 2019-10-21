ba9dfffebb95c8a42e49a43f02b9d773890880a9
Ek <ekng1999@gmail.com>
2019-10-21 14:05:54 +0800

    Practical 1.3

--- Practical1/src/main/java/com/example/test1/MainActivity.kt
+++ Practical1/src/main/java/com/example/test1/MainActivity.kt
@@ -6,39 +6,56 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var diceImage:ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton: Button = this.findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        val rollButton1: Button = findViewById(R.id.roll_button1)
+        rollButton1.setOnClickListener { countup() }
 
     }
 
+    private fun countup() {
+        val result1: TextView = findViewById(R.id.result_text)
+        val result2: ImageView = findViewById(R.id.dice_image1)
+        val result3: ImageView = findViewById(R.id.dice_image2)
+
+        if (result1.text.toString().toInt() <= 5) {
+            var resultInt = result1.text.toString().toInt()
+            resultInt++
+            result1.text = resultInt.toString()
+        }
+    }
+
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
         val diceImage1: ImageView = findViewById(R.id.dice_image1)
         val diceImage2: ImageView = findViewById(R.id.dice_image2)
 
+        //Coding Challenges for 1.2
+        resultText.text = "1"
 
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when (randomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        diceImage.setImageResource(drawableResource)
-
+//        val randomInt = Random().nextInt(6) + 1
+//        val drawableResource = when (randomInt) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+//        diceImage.setImageResource(drawableResource)
+//
         val randomInt1 = Random().nextInt(6) + 1
         val drawableResource1 = when (randomInt1) {
             1 -> R.drawable.dice_1
